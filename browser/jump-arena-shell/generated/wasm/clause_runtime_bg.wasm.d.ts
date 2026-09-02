cc3830653fec3188c723f87aa2f4a1c4f4b29993
tompassarelli <tom.passarelli@protonmail.com>
2026-09-01 23:52:24 -0700

    Rebuild Wasm with shared carrier terms

--- browser/jump-arena-shell/generated/wasm/clause_runtime_bg.wasm.d.ts
+++ browser/jump-arena-shell/generated/wasm/clause_runtime_bg.wasm.d.ts
@@ -1,11 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export const clause_process_v1_dispatch: () => number;
-export const clause_process_v1_request_push: (a: number) => number;
-export const clause_process_v1_reset: () => void;
-export const clause_process_v1_response_byte: (a: number) => number;
-export const clause_process_v1_response_len: () => number;
 export const clause_session_v1_command: () => number;
 export const clause_session_v1_command_bulk: (a: number, b: number) => number;
 export const clause_session_v1_event_bulk: () => [number, number];
@@ -22,6 +17,11 @@ export const clause_branch_v1_event_len: () => number;
 export const clause_branch_v1_io_reset: () => void;
 export const clause_branch_v1_open: () => number;
 export const clause_branch_v1_request_push: (a: number) => number;
+export const clause_process_v1_dispatch: () => number;
+export const clause_process_v1_request_push: (a: number) => number;
+export const clause_process_v1_reset: () => void;
+export const clause_process_v1_response_byte: (a: number) => number;
+export const clause_process_v1_response_len: () => number;
 export const __wbindgen_exn_store: (a: number) => void;
 export const __externref_table_alloc: () => number;
 export const __wbindgen_externrefs: WebAssembly.Table;
